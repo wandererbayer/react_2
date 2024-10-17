dcd5b737ae790f9e7f8fe53a29699ffdd99bd1b9
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2024-10-17 16:51:46 -0300

    Aula useState

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -5,29 +5,79 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [string, setString] = useState(false)
+  const [theme, setTheme] = useState(true)
+  const [balacocaco, setBalaco] = useState(true)
+
+  if(string == "Senai")
+  {
+    return alert("Apareci!")
+  }
 
   return (
     <>
-      <div>
+      {/* <button onClick={()=> setTheme(theme ? false : true)}>MODE</button>
+      {theme ?
+      <div className='fundo-preto'>
+        <h1>HELL</h1>
+        <input type="text" onChange={(e) => setName(e.target.value)} />
+      </div>
+      :
+      <div className='fundo-branco'>
+        <h1>HELL</h1>
+        <input type="text" onChange={(e) => setName(e.target.value)} />
+      </div>
+      } */}
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      </div>
-      <h1>Vite + React</h1>
+      </div> */}
+
+      <button onClick={()=> setTheme(theme ? false : true)}>Mode</button>
+      {theme ?
+        <body id="fundo-preto">
+        </body>
+        :
+        <body id="fundo-branco">
+        </body>
+      }
+
+      <button onClick={()=> setBalaco(balacocaco ? false : true)}>Clique</button>
+      {balacocaco ?
+        <div className="card">
+          <h1>Balacobaco</h1>
+        </div>
+        :
+        <div className="card">
+          <h1></h1>
+        </div>
+      }
+      
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count) => count + 10)}>
           count is {count}
         </button>
-        <p>
+      </div>
+      <div className="card">
+        <button onClick={() => setCount((count) => count - 10)}>
+          count is {count}
+        </button>
+      </div>
+      <div className="card">
+        <h2>{string}</h2>
+        <input type="text" onChange={(e) => setString(e.target.value)}/>
+      </div>
+        {/* <p>
           Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        </p> 
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }
--- React/src/main.jsx
+++ React/src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import HelloWorld from './HelloWorld.jsx'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-    <HelloWorld />
+    <App />
 )
